c722d405a53c286aba1163abeb084d584e11d9f5
cuichenxi <784759481@qq.com>
2017-08-08 16:30:29 +0800

    update

--- platforms/android/uwx/src/main/java/com/ucar/weex/commons/util/DevOptionHandler.java
+++
@@ -1,5 +0,0 @@
-package com.ucar.weex.commons.util;
-
-public interface DevOptionHandler {
-  void onOptionSelected();
-}
--- platforms/android/uwx/src/main/java/com/ucar/weex/utils/permission/SinglePermissionListener.java
+++
@@ -1,8 +0,0 @@
-package com.ucar.weex.utils.permission;
-
-/**
- */
-public interface SinglePermissionListener {
-    void onPermissionGranted(int requestCode, String[] permissions);
-    void onPermissionDenied(int requestCode, String[] permissions);
-}
